0ce97d7ae33a2b2449e2d85965c955498d99ca3f
Nacer / <nacerlachi5@gmail.com>
2025-10-30 11:50:21 +0100

    Update fxmanifest.lua

--- nacer-ui/fxmanifest.lua
+++ nacer-ui/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Nacer4Dev'
-description 'Modern UI Scipt "Server Logo + Server Name'
+description 'Modern UI Scipt "Server Logo + Server Name"'
 version '1.5.1'
 
 ui_page 'html/index.html'
@@ -16,4 +16,5 @@ files {
 
 client_scripts {
     'client.lua'
+
 }
